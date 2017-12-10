e6aa38cb3f7509fc0a080cad0a60c04b59d22d47
quyuanhang <qu_yh@pku.edu.cn>
2017-12-10 21:38:20 +0800

    update csvd

--- onlline_social_transfer/csvd.py
+++ onlline_social_transfer/csvd.py
@@ -3,82 +3,69 @@ import tensorflow as tf
 import pandas as pd
 import numpy as np
 from scipy import sparse
+import DYP
+import test
 
-def print_schedule(begin, i, s_=None):
-    if not s_:
-        return 0
-    if i % 1000 == 0:
-        sum_time = '%0.2f' % (time.time() - begin)
-        sys.stderr.write(("\r%s %d sum time %s" % (s_, i, sum_time)))
-        sys.stderr.flush()
-
-
-def complete_schedual():
-    sys.stderr.write("\n")
-    sys.stderr.flush()
 
 train_male = pd.read_csv('input/male_train.csv', header=None).values
 train_female = pd.read_csv('input/female_train.csv', header=None).values
-
 male_set = set(train_male[:, 0]) & set(train_female[:, 0])
 female_set = set(train_male[:, 1]) & set(train_female[:, 1])
-
 male_index_dict = dict(zip(male_set, range(len(male_set))))
 female_index_dict = dict(zip(female_set, range(len(female_set))))
 
-male_matrix = np.zeros([len(male_set), len(female_set)])
-for row in train_male:
-    male, female, rate = row
-    if male in male_index_dict and female in female_index_dict:
-        male_matrix[male_index_dict[male], female_index_dict[female]] = rate
-male_matrix_position = male_matrix.copy()
-male_matrix_position[male_matrix_position>0] = 1
+train_male = np.array([[male_index_dict[i[0]], female_index_dict[i[1]], i[2]]
+    for i in train_male if i[0] in male_index_dict and i[1] in female_index_dict])
+train_female = np.array([[male_index_dict[i[0]], female_index_dict[i[1]], i[2]] 
+    for i in train_female if i[0] in male_index_dict and i[1] in female_index_dict])
+n_users, n_items = len(male_set), len(female_set)
 
 
-female_matrix = np.zeros([len(male_set), len(female_set)])
-for row in train_female:
-    male, female, rate = row
-    if male in male_index_dict and female in female_index_dict:
-        female_matrix[male_index_dict[male], female_index_dict[female]]
-female_matrix_position = female_matrix.copy()
-female_matrix_position[female_matrix_position>0] = 1
-
-
-male_num, female_num = len(male_set), len(female_set)
-feature_num = 5
-optimizer = tf.train.AdadeltaOptimizer(1)
-
-
-mat_male_feature = tf.Variable(tf.random_normal((male_num, feature_num)) / tf.sqrt(float(feature_num)))
-mat_feature_female = tf.Variable(tf.random_normal((feature_num, female_num)) / tf.sqrt(float(feature_num)))
-feature_vec_male = tf.Variable(tf.random_normal(shape=[feature_num,]))
-mat_feature = tf.diag(feature_vec_male)
-feature_vec_female = tf.Variable(tf.random_normal(shape=[feature_num,]))
-mat_feature_ = tf.diag(feature_vec_female)
-
-male_prediction = tf.matmul(tf.matmul(mat_male_feature, mat_feature), mat_feature_female)
-female_prediction = tf.matmul(tf.matmul(mat_male_feature, mat_feature_), mat_feature_female)
-
-loss = tf.reduce_sum(tf.pow(tf.subtract(male_prediction * male_matrix_position, male_matrix), 2.0)) \
-     + tf.reduce_sum(tf.pow(tf.subtract(female_prediction * female_matrix_position, female_matrix), 2.0))
-
-
-init = tf.global_variables_initializer()
-sess = tf.InteractiveSession()
-sess.run(init)
-
+model = DYP.CML(n_users, n_items, 5, 1)
 
 begin = time.time()
 l_loss = 0
 i = 0
-while True:
-    sess.run(optimizer.minimize(loss))
-    c_loss = tf.eval(loss)
-    if abs(c_loss - l_loss) < 100:
+stop = 0
+for i in tqdm(range(500)):
+    c_loss = model.partial_fit(train_male[:, [0, 1]], train_male[:, [2]], 
+                        train_female[:, [0, 1]], train_female[:, [2]])    
+    if abs(c_loss - l_loss) == 0:
+        stop += 1
+    else:
+        stop =0
+    if stop >= 100:
         break
     l_loss = c_loss
-    print_schedule(begin, i, ('loss function %0.1f' % c_loss))
+    sys.stderr.write(("loss function %f" % (c_loss)))
+    sys.stderr.flush()
     i += 1
-complete_schedual()
+
+male_prediction = model.prediction_matrix()
+
+test_data = pd.read_csv('input/male_test.csv', header=None).values
+# male_test_raw[:, 2] = 2 #计算单边auc
+
+
+test_data = np.array([[male_index_dict[i[0]], female_index_dict[i[1]], i[2]]
+    for i in test_data if i[0] in male_index_dict and i[1] in female_index_dict])
+test.user_auc(male_prediction, train_male, test_data)
+
+p_array = np.array(list(map(lambda x: male_prediction[x[0], x[1]], test_data)))
+test_y = test_data[:, 2]
+print(test.sample_auc(p_array, test_y, 1))
+
+
+train_dict = test.data_to_dict(train_male, 0)
+test_dict = test.data_to_dict(test_data, 0)
+
+precision_list, recall_list = [], []
+for k in [5, 10, 50]:
+    precision, recall = test.precision_recall(male_prediction, test_dict, train_dict, top=k, mode='base').values[0]
+    precision_list.append(precision)
+    recall_list.append(recall)
+
+plt.scatter(precision_list, recall_list)
+plt.show()
 
 
---
+++ onlline_social_transfer/test.py
@@ -0,0 +1,109 @@
+# 內建库
+import sys
+# 第三方库
+import pandas as pd
+import numpy as np
+
+def user_auc(prediction_mat, train_data, test_data, s=0.3):
+    def _data_to_dict(data):
+        pos_dict = dict()
+        match_dict = dict()
+        all_items = set()
+        for (user, item, rate) in data:
+            if rate == 2:
+                if user not in match_dict:
+                    match_dict[user] = list()
+                match_dict[user].append(item)
+            else:
+                if user not in pos_dict:
+                    pos_dict[user] = list()
+                pos_dict[user].append(item)
+            all_items.add(item)
+        return match_dict, pos_dict, (set(match_dict.keys()) & set(pos_dict.keys())), all_items
+    train_match_dict, train_pos_dict, train_users, train_items = _data_to_dict(train_data)    
+    test_match_dict, test_pos_dict, test_users, test_items = _data_to_dict(test_data)
+    auc_values = []
+    z = 0
+    user_array = np.array(list(test_users & train_users))
+    user_sample = user_array[np.random.randint(len(user_array), size=round(s * len(user_array)))]
+# =============================================================================
+#     user_sample = user_array
+# =============================================================================
+    for user in user_sample:
+        auc_for_user = 0.0
+        n = 0
+        predictions = prediction_mat[user]
+        match_items = set(test_match_dict[user]) & train_items - set(train_match_dict[user])
+        pos_items = set(test_pos_dict[user]) & train_items - set(train_pos_dict[user])
+        neg_items = train_items - match_items - pos_items - set(train_pos_dict[user]) - set(train_match_dict[user])
+        for match_item in match_items:
+            for other_item in pos_items | neg_items:
+                n += 1
+                if predictions[match_item] > predictions[other_item]:
+                    auc_for_user += 1
+                elif predictions[match_item] == predictions[other_item]:
+                    auc_for_user += 0.5
+        if n > 0:
+            auc_for_user /= n
+            auc_values.append(auc_for_user)
+        z += 1
+        if z % 10 == 0 and len(auc_values) > 0:
+            sys.stderr.write("\rCurrent AUC mean (%s samples): %0.3f" % (str(z), np.mean(auc_values)))
+            sys.stderr.flush()
+    sys.stderr.write("\n")
+    sys.stderr.flush()
+    return np.mean(auc_values)
+
+def sample_auc(p_array, test_y, split):
+    positive_index = [i[0] for i in enumerate(test_y) if i[1] >= split]
+    negative_index = [i[0] for i in enumerate(test_y) if i[1] < split]
+    positive_score = p_array[positive_index]
+    negative_score = p_array[negative_index]
+    auc = 0.0
+    for pos_s in positive_score:
+        for neg_s in negative_score:
+            if pos_s > neg_s:
+                auc += 1
+            if pos_s == neg_s:
+                auc += 0.5
+    auc /= (len(positive_score) * len(negative_score))
+    return auc
+
+def precision_recall(recommend, lable_dict, train_dict, top=1000, mode='base', sam=0.3):
+    tp, fp, fn = 0, 0, 0
+    precision_recall_list = list()
+    user_array = np.array(list(set(lable_dict.keys()) & set(train_dict.keys())))
+    user_sample = user_array[np.random.randint(len(user_array), size=round(sam * len(user_array)))]
+    for exp in user_sample:
+        job_rank_list = recommend[exp]
+        job_rank = sorted(enumerate(job_rank_list), key=lambda x: x[1], reverse=True)
+        rec = [j_r[0] for j_r in job_rank if j_r[0] not in train_dict[exp]][:top]
+        rec_set = set(rec)
+        positive_set = set(lable_dict[exp].keys()) - set(train_dict[exp].keys())
+        tp += len(rec_set & positive_set)
+        fp += len(rec_set - positive_set)
+        fn += len(positive_set - rec_set)
+        if len(positive_set) > 0:
+            if mode == 'max':
+                precision = 1 if rec_set & positive_set else 0
+                recall = 1 if rec_set & positive_set else 0
+            else:
+                precision = len(rec_set & positive_set) / (len(rec_set) + 0.01)
+                recall = len(rec_set & positive_set) / (len(positive_set) + 0.01)
+            precision_recall_list.append([precision, recall])
+    if (mode == 'base') or (mode == 'max'):
+        df = pd.DataFrame(precision_recall_list, columns=[
+                          'precision', 'recall'])
+        return pd.DataFrame([df.mean(), df.std()], index=['mean', 'std'])
+    elif mode == 'sum':
+        return ('precision, recall \n %f, %f' % ((tp / (tp + fp)), (tp / (tp + fn))))
+
+def data_to_dict(training_data, min_rate):
+    train_dict = dict()
+    for row in training_data:
+        user, item, rate = row
+        if rate >= min_rate:
+            if user not in train_dict:
+                train_dict[user] = dict()
+            train_dict[user][item] = rate
+    return train_dict
